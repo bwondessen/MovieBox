305c82beaa7415a0aa302d90a1613c530a29dfd9
bwondessen <bwondessen23@gmail.com>
2024-12-22 22:25:37 -0600

    Create MovieCard component

---
+++ moviebox/src/components/MovieCard.jsx
@@ -0,0 +1,22 @@
+function MovieCard({movie}) {
+    function favorite() {
+        alert("Favorited!")
+    }
+
+    return (
+        <div className="movie-card">
+            <div class="movie-poster">
+                <img src="{movie.url}" alt="{movie.title}" />
+                <div class="movie-overlay">
+                     <button class="favorite-btn" onClick={favorite}>🤍</button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
