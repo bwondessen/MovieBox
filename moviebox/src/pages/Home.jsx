91a4e54102be3cd39d42171bfca662293293bd18
bwondessen <bwondessen23@gmail.com>
2024-12-22 23:23:35 -0600

    Create form

--- moviebox/src/pages/Home.jsx
+++ moviebox/src/pages/Home.jsx
@@ -7,8 +7,16 @@ function Home() {
         { id: 3, title: "The Matrix", release_date: "1999" }
     ];
 
+    const handleSearch = () => {
+
+    }
+
     return (
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for movies..." className="search-input" />
+                <button type="submit" className="search-button">Search</button>
+            </form>
             <div className="movies-grid">
                 {movies.map(movie => <MovieCard movie={movie} key={movie.id} />)}
             </div>
