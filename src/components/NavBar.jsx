fd7de64bf5a5112f37d77a1746aa490a48f04065
bwondessen <bwondessen23@gmail.com>
2024-12-25 19:32:46 -0600

    Update style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/">Movie App</Link>
+                <Link to="/" className="site-title">MovieBox</Link>
             </div>
             <div className="navbar-links">
                 <Link to="/" className="nav-link">Home</Link>
